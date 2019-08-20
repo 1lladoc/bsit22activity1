0b08ca316aa7bb89d38f5b40c7ea2d36b7871db1
uxer <uxer@DESKTOP-UDT7E24>
2019-08-20 13:04:10 +0800

    added classes with methods

---
+++ src/login.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uxer
+ */
+public class login {
+    
+    public static void login(String username, String password){
+        System.out.println("Hello "+username);
+    }
+    
+}
